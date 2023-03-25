9bfae693c806c1e067a7b7f9a666d4c1ccee634c
jzicc <123297148+jzicc@users.noreply.github.com>
2023-03-25 23:23:18 +0800

    2 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  home = 'Welcome to our Spring Sale on books! We`re offering great discounts on a variety of books from genres like fiction, non-fiction, romance, fantasy, mystery, science-fiction and more. Get up to 50% off on select books, plus get free shipping on orders over $50. Don’t miss out on this chance to stock up on your favorite titles at great prices. Hurry and shop now, our sale won`t last forever!';
+
+  offer = 'We offer a wide selection of books for readers of all ages, from classic novels to contemporary bestsellers. Our selection also includes books from a variety of genres, including fiction, non-fiction, children`s, fantasy, mystery, science-fiction, and more. In addition to books, we also offer a selection of magazines and audiobooks. We also offer helpful customer service and competitive prices to ensure that you get the best value for your money.'
+
+  Title = 'To Kill a Mockingbird'
+  Author = 'Harper Lee'
+  Publisher = 'J. B. Lippincott & Co.'
+  Category = 'Southern Gothic fiction'
+  Price = '$21.03'
+  Desc = 'To Kill a Mockingbird is both a young girls coming-of-age story and a darker drama about the roots and consequences of racism and prejudice, probing how good and evil can coexist within a single community or individual.'
+
+
+  
   constructor(private router: Router) {}
 
   goToCart(pageName:string):void{
@@ -14,5 +27,8 @@ export class HomeComponent implements OnInit {
 
   }
 
-  ngOnInit() {}
+  ngOnInit(): void {}
 }
+
+
+
